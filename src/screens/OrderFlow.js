7d04b7fad0a7785c9e80cf52031ba5b0d2b0a711
Eugene Turchik <eturchik@zaraffasoft.com>
2021-02-21 04:10:07 +0200

    Added driver flow page

--- src/screens/OrderFlow.js
+++ src/screens/OrderFlow.js
@@ -5,14 +5,21 @@ import SecureStorage from "react-native-secure-storage";
 import {Button} from "../components";
 import getTheme from "../global/Style";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {useNavigation} from "@react-navigation/native";
 
-let ordersWatch = null;
-
-export default function OrderFlow() {
+export default function OrderFlow(props) {
     const theme = getTheme();
     const styles = getStyles(theme);
+    const {navigate} = useNavigation();
+    const {order} = props.route.params;
     const [isConfirmed, setConfirmed] = React.useState(false);
+    const [isUserConfirmed, setUserConfirmed] = React.useState(true);
+    const [isMovingOut, setMovingOut] = React.useState(false);
+    const [isInPosition, setInPosition] = React.useState(false);
+    const [isFinished, setFinished] = React.useState(false);
 
+
+    console.log(order);
     // const refreshOrders = React.useCallback(() => {
     //     SecureStorage.getItem('token')
     //         .then(token => {
@@ -26,40 +33,99 @@ export default function OrderFlow() {
     // });
 
     React.useEffect(() => {
-        refreshOrders();
-        ordersWatch = setInterval(refreshOrders, 3000);
-        return () => clearInterval(ordersWatch)
+
     }, []);
 
-    return <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-            {orders.map((order, idx) => {
-                return <View key={idx} style={styles.orderRow}>
-                    <Text style={{flex: 1}}>{`From ${order.from} to ${order.to}`}</Text>
-                    <Button containerStyle={styles.orderButton}
-                            text={'Select'}
+    // TODO Add user confirmation poll
+    let userText = isUserConfirmed ? 'User confirmed your offer' : 'Awaiting passenger confirmation...';
+
+    return <View>
+        {!isConfirmed ? (
+            <View style={styles.container}>
+                <Text>Do you want to confirm taking this order?</Text>
+                <View style={{flexDirection: 'row', marginTop: theme.scale(10)}}>
+                    <Button containerStyle={styles.confirmButton}
+                            text={'No'}
                             buttonColor={theme.textSecondary}
                             textColor={theme.black}
+                            onPress={() => navigate('OrderList')}
+                    />
+                    <Button containerStyle={styles.confirmButton}
+                            text={'Yes'}
+                            buttonColor={theme.textSecondary}
+                            textColor={theme.black}
+                            onPress={() => setConfirmed(true)}
                     />
                 </View>
-            })}
-        </ScrollView>
-    </SafeAreaView>;
+            </View>
+        ) : (
+            <View style={styles.container}>
+                <Text>Step 1</Text>
+                <Text>{userText}</Text>
+                {isUserConfirmed && <>
+                    <Text>Step 2</Text>
+                    {!isMovingOut ?
+                        <>
+                            <Text>Click the button when you are ready to go</Text>
+                            <Button text={'I\'m on my way!'}
+                                    buttonColor={theme.textSecondary}
+                                    textColor={theme.black}
+                                    onPress={() => setMovingOut(true)}
+                            />
+                        </>
+                        :
+                        <Text>You've confirmed you're on your way to passenger</Text>
+                    }
+                    {isMovingOut && <>
+                        <Text>Step 3</Text>
+                        {!isInPosition ?
+                            <>
+                                <Text>Click the button when you are in position</Text>
+                                <Button text={'I\'m in position!'}
+                                        buttonColor={theme.textSecondary}
+                                        textColor={theme.black}
+                                        onPress={() => setInPosition(true)}
+                                />
+                            </>
+                            :
+                            <Text>You've confirmed your arrival. Wait for passenger to come.</Text>
+                        }
+                        {isInPosition && <>
+                            <Text>Step 4</Text>
+                            {!isFinished ?
+                                <>
+                                    <Text>Click the button if you've finished the trip.</Text>
+                                    <Button text={'Finish!'}
+                                            buttonColor={theme.textSecondary}
+                                            textColor={theme.black}
+                                            onPress={() => setFinished(true)}
+                                    />
+                                </>
+                                :
+                                <Text>You've confirmed the trip end.</Text>
+                            }
+                            </>}
+                        </>}
+                </>}
+            </View>
+        )}
+    </View>;
 }
 
 function getStyles(theme) {
     return {
-        orderRow: {
+        container: {
             display: "flex",
-            flexDirection: "row",
-            justifyContent: "flex-start",
+            flexDirection: "column",
+            justifyContent: "center",
             alignItems: "center",
             width: "100%",
+            height: "100%",
             padding: theme.scale(12),
         },
-        orderButton: {
-            flex: 0.4,
-            paddingHorizontal: theme.scale(0),
+        confirmButton: {
+            //flex: 1,
+            marginHorizontal: theme.scale(10),
         },
     };
 }
